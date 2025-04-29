712ff46f37fd5254242a83318af960beb3be64a3
mannantanwar <mannantanwar@gmail.com>
2025-04-29 13:37:31 +0530

    29/04/2025

--- codeforces/C_Card_Game.cpp
+++ codeforces/C_Card_Game.cpp
@@ -1,39 +1,49 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--)
+    {
         int n;
-        cin>>n;
-        vector<int>alice;
-        vector<int>bob;
-        for(int i=0;i<n;i++){
+        cin >> n;
+        unordered_map<int, int> A;
+        unordered_map<int, int> B;
+        for (int i = 0; i < n; i++)
+        {
             char a;
-            cin>>a;
-            if(a=='A')alice.push_back(i);
-            else bob.push_back(i);
+            cin >> a;
+            if (a == 'A')
+                A[i]++;
+            else
+                B[i]++;
         }
-        unordered_map<int,int>A;
-        unordered_map<int,int>B;
-        for( int i=0;i<alice.size();i++){
-            A[alice[i]]++;
+        if (A.count(0) && A.count(n - 1))
+        {
+            cout << "Alice" << endl;
         }
-        for( int i=0;i<bob.size();i++){
-            B[bob[i]]++;
+        else if (B.count(0) && B.count(n - 1))
+        {
+            cout << "Bob" << endl;
         }
-        while(!alice.empty() && !bob.empty()){
-            int a = alice.size();
-            int b = bob.size();
-            if(A.count(0) && A.count(n-1)){
-                cout<<"Alice"<<endl;
+        else
+        {
+            // 0 and n-1 card alag alag bndo par hai
+            if (A.count(0) && B.count(n - 1))
+            {
+                if (B.size() > 1)
+                    cout << "Bob" << endl;
+                else
+                    cout << "Alice" << endl;
             }
-            else if(B.count(0) && B.count(n-1)){
-                cout<<"Bob"<<endl;
-            }
-            else{
-                //0 and n-1 card alag alag bndo par hai
-                if(A.count(n-1) && )
+            else
+            {
+                // alice has n-1 and bob has 0
+                if (A.count(n - 2))
+                    cout << "Alice" << endl;
+                else
+                    cout << "Bob" << endl;
             }
         }
     }
